d7ddb72c66fde41d417e4abd82890b191bffbb2b
Laurine BRETTE <labrette>
2023-01-27 15:33:00 +0100

    controller champion

---
+++ Sources/API_lol/Controllers/ChampionControllers.cs
@@ -0,0 +1,52 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using StubLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_lol.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChampionControllers : ControllerBase
+    {
+        private readonly IDataManager _dataManager;
+
+        public ChampionControllers(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var stub = new StubData();
+
+            var dto = new ChampionDTO();
+            var lesChampions = new List<Champion>();
+
+            return Ok(lesChampions);
+        }
+
+        // des exemples
+
+        // POST api/<ChampionController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ChampionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ChampionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Sources/API_lol/Mapper/ChampionMapper.cs
@@ -0,0 +1,22 @@
+﻿namespace API_lol.Mapper
+{
+    using Model;
+    using DTO;
+
+    public static class ChampionMapper
+    {
+
+        // Transformer un champion en champion DTO 
+        // ex enlever le mot de pass pour pas que le client le récupère
+        public ChampionDTO ToDTO(this Champion champion)
+        {
+            var championDTO = new ChampionDTO()
+            {
+               Name = champion.Name,
+               Bio = champion.Bio,
+
+            }
+            return championDTO;
+        }
+    }
+}
