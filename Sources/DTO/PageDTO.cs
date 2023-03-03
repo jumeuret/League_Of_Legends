747cf5f640e05a01b232775bdbdc155d105f6ae6
jumeuret <justine.meuret@etu.uca.fr>
2023-03-03 15:55:38 +0100

    Ajout de la pagination sur le GetChampions

---
+++ Sources/DTO/PageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO;
+
+public class PageDTO<T>
+{
+    public T Data { get; set; }
+    public int Index { get; set; }
+    public int Count { get; set; }
+    public int TotalCount { get; set; }
+}
