a223eda57446d89793d55899cfa674b090ff7113
jumeuret <justine.meuret@etu.uca.fr>
2023-03-03 16:07:12 +0100

    Ajout .drone.yml

---
+++ Sources/DTO/SkillDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace DTO;
+
+public class SkillDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+
+    public SkillDTO(string Name, string Description, string Type)
+    {
+        this.Name = Name;
+        this.Description = Description;
+        this.Type = Type;
+    }
+}
